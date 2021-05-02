0e3a80d23ff087c716d00bf226ef214a1aed6d68
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-05-02 23:12:12 +0200

    Add Moving Platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _pointA, _pointB;
+    private Vector3 _target; 
+    [SerializeField]
+    private float _speed;
+    private bool _isMovingToA = false;
+
+    void Update()
+    {
+        _target = transform.position == _target ? _target == _pointA.position ? _pointB.position : _pointA.position : _target;
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+    }
+}
