747ea8ac2db191dabf66937ac4381c5e232498f4
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-05-08 20:21:00 +0200

    Attach player to moving platform when landing on it

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -6,14 +6,35 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField]
     private Transform _pointA, _pointB;
-    private Vector3 _target; 
+    private Vector3 _target;
     [SerializeField]
     private float _speed;
-    private bool _isMovingToA = false;
 
-    void Update()
+    void Start()
+    {
+        _target = _pointA.position;
+    }
+
+    void FixedUpdate()
     {
         _target = transform.position == _target ? _target == _pointA.position ? _pointB.position : _pointA.position : _target;
         transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+    }
+
+    // Because we're using Character controller, the Trigger must be large enough for the character controller hitbox to notice the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
     }
 }
